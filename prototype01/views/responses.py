e1d34d61b73e4f2f927deed92c10a5353c0125a2
Marcus Brumfield <brumfieldmarcus@gmail.com>
2015-04-15 19:39:11 -0500

    added method for creating response
    Method created for the create method for responses.py

--- prototype01/views/responses.py
+++ prototype01/views/responses.py
@@ -20,7 +20,20 @@ def get_response():
 # create
 @mod.route('/responses', methods=['POST'])
 def create_response():
-    return jsonify({'response': []})
+    if not (request.args['question'] and request.args['answer'] and request.args['creator']):
+        abort(404)
+    id = request.args['question']
+    question_response = request.args['answer']
+    flashcard_id = request.args['creator']
+    user_id = models.User.query.filter_by(id=creator).first()
+    if not user_id:
+        print 'User with id: %d not found' % creator
+        abort(404)
+    response = models.FlashCardResponse(question_text=question, question_answer=answer, user=user_id)
+    models.db_session.add(response)
+    models.db_session.commit()
+    return jsonify({'response': {'id': response.id, 'question_response': response.question_response,
+                                  'flashcard_id': response.flashcard_id, 'user_id': response.user_id}})
 
 # update
 @mod.route('/responses/<id>', methods=['POST'])
